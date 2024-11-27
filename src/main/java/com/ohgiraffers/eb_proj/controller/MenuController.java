41f452c7ec823d895580159f1962c97ee6e32cd6
ygc1994 <ygc1994@naver.com>
2024-11-27 20:39:05 +0900

    commit

--- src/main/java/com/ohgiraffers/eb_proj/controller/MenuController.java
+++ src/main/java/com/ohgiraffers/eb_proj/controller/MenuController.java
@@ -3,10 +3,12 @@ package com.ohgiraffers.eb_proj.controller;
 import com.ohgiraffers.eb_proj.dto.MenuDTO;
 import com.ohgiraffers.eb_proj.service.MenuService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("*")
 @RestController
 @RequiredArgsConstructor
 public class MenuController {
